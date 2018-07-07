8cbc22164acb956345797f6985519cfed7f71d61
deepakkarki <deepak6k@gmail.com>
2018-07-07 09:06:38 +0530

    render the hearts page

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,8 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage)
 
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
+
 //API
 router.get('/api/search', catchErrors(storeController.searchStores))
 router.get('/api/stores/near', catchErrors(storeController.mapStores))
